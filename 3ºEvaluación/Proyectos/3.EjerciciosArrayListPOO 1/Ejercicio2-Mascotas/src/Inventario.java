120599e7592f52ad940fb075dd19d630ca911da9
Snakeyesmp <snakeyes271101@gmail.com>
2023-03-25 00:09:18 +0100

    Clases animales añadidas,main empezado, pdfs modificados.

--- 3ºEvaluación/Proyectos/3.EjerciciosArrayListPOO 1/Ejercicio2-Mascotas/src/Inventario.java
+++ 3ºEvaluación/Proyectos/3.EjerciciosArrayListPOO 1/Ejercicio2-Mascotas/src/Inventario.java
@@ -1,10 +1,22 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Inventario {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        
+        ArrayList<Mascota> listadoAnimales = new ArrayList<Mascota>();
+
+        Mascota perro1 = new Perro(null, false);
+        Mascota doraemon = new Gato("Azul", false);
+        Mascota loro1 = new Loro("Amarillo", true, "Uganda", true);
+        Mascota canario1 = new Canario("Verde", true, "Marron", true);
+        listadoAnimales.add(perro1);
+        listadoAnimales.add(doraemon);
+        listadoAnimales.add(loro1);
+        listadoAnimales.add(canario1);
+
         // fechaNacimiento=LocalDate.of(2022,10,21);
 
+        System.out.println();
     }
 }
