1589c74427349224bd5f7e2ff51c885370a42a7f
Snakeyesmp <snakeyes271101@gmail.com>
2023-03-27 21:29:30 +0200

    Cosas hechas en clase de Encarna 27-03-2023

--- 3ºEvaluación/Proyectos/3.EjerciciosArrayListPOO 1/Ejercicio2-Mascotas/src/Inventario.java
+++ 3ºEvaluación/Proyectos/3.EjerciciosArrayListPOO 1/Ejercicio2-Mascotas/src/Inventario.java
@@ -1,56 +1,31 @@
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Inventario {
-    public static void main(String[] args) {
 
-        ArrayList<Mascota> listadoAnimales = new ArrayList<Mascota>();
-
-        Mascota perro1 = new Perro("Doberman", false);
-        Mascota doraemon = new Gato("Azul", false);
-        Mascota loro1 = new Loro("Amarillo", true, "Uganda", true);
-        Mascota canario1 = new Canario("Verde", true, "Marron", true);
-        listadoAnimales.add(perro1);
-        listadoAnimales.add(doraemon);
-        listadoAnimales.add(loro1);
-        listadoAnimales.add(canario1);
-
-        // fechaNacimiento=LocalDate.of(2022,10,21);
-
-        System.out.println(
-                "1. Mostrar la lista de animales\n2.Mostrar todos los datos en un animal concreto\n3.Mostrar todos los datos de todos los animales\n4.Insertar animales en el inventario\n5.ELiminar animales del inventario\n6.Vaciar el inventario");
-
-        Scanner sc = new Scanner(System.in);
-        int opcion = sc.nextInt();
-
-        switch (opcion) {
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-            case 3:
-
-                System.out.println("Datos de todos los animales: ");
-                for (Mascota mascota : listadoAnimales) {
-                    System.out.println(mascota);
-                }
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-                listadoAnimales.clear();
-                System.out.println("El inventario ha sido vaciado correctamente");
-                break;
-            default:
-                break;
-        }
-
+    public static void eliminarAnimales(ArrayList<Mascota> listadoAnimales) {
+        listadoAnimales.clear();
+        System.out.println("El inventario ha sido vaciado correctamente");
     }
+
+    public static void datosDeTodosLosAnimales(ArrayList<Mascota> listadoAnimales) {
+        System.out.println("Datos de todos los animales: ");
+        for (Mascota mascota : listadoAnimales) {
+            System.out.println(mascota);
+        }
+    }
+
+    public static void eliminarUnAnimal(ArrayList<Mascota> listadoAnimales, Mascota nombre) {
+        for (Mascota mascota : listadoAnimales) {
+            if (mascota.getNombre().equals(nombre)) {
+                listadoAnimales.remove(mascota);
+                System.out.println("Animal eliminado: " + mascota);
+            }
+        }
+    }
+
+    public String getTipoMascota(Mascota mascota) {
+        
+        return mascota.getClass().getSimpleName();
+    }
+
 }
