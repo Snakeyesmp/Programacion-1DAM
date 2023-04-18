9db4a85da18112098067399513ba49cfcc84b259
Snakeyesmp <snakeyes271101@gmail.com>
2023-04-18 17:40:05 +0200

    Ejercicio 1 terminado. Inicio ejercicio 2

---
+++ 3ºEvaluación/Proyectos/9.FicherosB/src/Ejercicio01.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Implementa un programa que muestre por pantalla los valores máximos y mínimos
+ * del archivo ‘numeros.txt’.
+ */
+public class Ejercicio01 {
+    public static void main(String[] args) {
+
+        File archNumeros = new File("Documentos/numeros.txt");
+        int numMax = 0, numMin = 999999;
+
+        try {
+            // Scanner en el archivo en el que se encuentran los numeros
+            Scanner sc = new Scanner(archNumeros);
+
+            while (sc.hasNextInt()) {
+                int numero = sc.nextInt();
+                if (numero > numMax) {
+                    numMax = numero;
+                }
+                if (numero < numMin) {
+                    numMin = numero;
+                }
+            }
+
+            sc.close();
+
+            System.out.println("Valor máximo: " + numMax);
+            System.out.println("Valor mínimo: " + numMin);
+
+        } catch (FileNotFoundException e) {
+            // Excepcion en caso de que el archivo especificado en la ruta no se encuentre
+            System.out.println("El archivo no se ha encontrado");
+        }
+    }
+
+}
---
+++ 3ºEvaluación/Proyectos/9.FicherosB/src/Ejercicio02.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * El archivo ‘alumnos_notas.txt’ contiene una lista de 10 alumnos y las notas
+ * que han obtenido en cada asignatura. El número de asignaturas de cada alumno
+ * es variable. Implementa un programa que muestre por pantalla la nota media de
+ * cada alumno junto a su nombre y apellido, ordenado por nota media de mayor a
+ * menor.
+ */
+public class Ejercicio02 {
+    public static void main(String[] args) {
+
+        File notasAlumnos = new File("documentos/alumnos_notas.txt");
+
+        try {
+
+            Scanner sc = new Scanner(notasAlumnos);
+            double notaSuma = 0;
+            double notaMedia = 0;
+            int numNotas = 0;
+            while (sc.hasNextInt()) {
+
+                notaSuma = notaSuma + sc.nextInt();
+                numNotas++;
+            }
+            notaMedia = (notaSuma / numNotas);
+
+            System.out.println(notaMedia);
+
+        } catch (FileNotFoundException e) {
+
+            System.out.println("El archivo no se ha encontrado");
+        }
+
+    }
+}
